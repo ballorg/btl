df21de19763bce1ee77a526c2c315c60b0774607
Vladimir Ezhikov <wend4r@gmail.com>
2025-11-22 16:29:14 +0300

    Make `IS_CONST` insensitive to pointers

--- include/ball/types/meta/isconst.hpp
+++ include/ball/types/meta/isconst.hpp
@@ -3,9 +3,8 @@
 
 // Determine whether T is a (raw) pointer type (excludes member pointers).
 template < typename T > constexpr bool IS_CONST = false;
-template < typename U > constexpr bool IS_CONST< U * > = true;
-template < typename U > constexpr bool IS_CONST< U * const > = true;
-template < typename U > constexpr bool IS_CONST< U * volatile > = false;
-template < typename U > constexpr bool IS_CONST< U * const volatile > = true;
+template < typename U > constexpr bool IS_CONST< U const > = true;
+template < typename U > constexpr bool IS_CONST< U volatile > = false;
+template < typename U > constexpr bool IS_CONST< U const volatile > = true;
 
 #endif // !defined( _INCLUDE_BALL_TYPES_META_ISCONST_HPP_ )
