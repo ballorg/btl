ec696fe3d33cb45ceb10c40198a6204e1e8ee3d7
Vladimir Ezhikov <wend4r@gmail.com>
2026-04-16 03:21:03 +0300

    Correct memory fucntion signatures

--- include/ball/types/c/memory.h
+++ include/ball/types/c/memory.h
@@ -3,19 +3,18 @@
 
 #	include "macros.h"
 
-#	ifdef _WIN32
-BALL_DLL_IMPORT_C unsigned long long _msize( void *pMem );
+#	if defined( BALL_MSVC )
+BALL_EXTERN_C unsigned long long __cdecl _msize( void *pMem );
+BALL_EXTERN_C void * __cdecl memset( void *pDest, int nFill, size_t nCount );
+BALL_EXTERN_C void * __cdecl memcpy( void *pDest, const void *pSrc, size_t nCount );
+BALL_EXTERN_C void * __cdecl memmove( void *pDest, const void *pSrc, size_t nCount );
 #	else
-BALL_DLL_IMPORT_C unsigned long long malloc_usable_size( void *pMem );
+BALL_EXTERN_C unsigned long long malloc_usable_size( void *pMem );
 inline unsigned long long _msize( void *pMem ) { return malloc_usable_size( pMem ); }
-#	endif // defined( _WIN32 )
 
-// BALL_DLL_IMPORT_C void *malloc( unsigned long long nSize );
-// BALL_DLL_IMPORT_C void *realloc( void *pMem, unsigned long long nSize );
-// BALL_DLL_IMPORT_C void free( void *pMem );
-
-BALL_DLL_IMPORT_C void *memset( void *pDest, int nFill, size_t nCount );
-BALL_DLL_IMPORT_C void *memcpy( void *pDest, const void *pSrc, size_t nCount );
-BALL_DLL_IMPORT_C void *memmove( void *pDest, const void *pSrc, size_t nCount );
+BALL_EXTERN_C void *memset( void *pDest, int nFill, size_t nCount );
+BALL_EXTERN_C void *memcpy( void *pDest, const void *pSrc, size_t nCount );
+BALL_EXTERN_C void *memmove( void *pDest, const void *pSrc, size_t nCount );
+#	endif // defined( BALL_MSVC )
 
 #endif // !defined( _INCLUDE_BALL_TYPES_C_MEMORY_H_ )
--- include/ball/types/c/memoryaligned.h
+++ include/ball/types/c/memoryaligned.h
@@ -3,9 +3,16 @@
 
 #	include "macros.h"
 
-BALL_DLL_IMPORT_C void *_aligned_malloc( unsigned long long nSize, unsigned long long nAlignment );
-BALL_DLL_IMPORT_C void _aligned_free( void *p );
-BALL_DLL_IMPORT_C unsigned long long _aligned_msize( void *p, unsigned long long nAlignment, unsigned long long nOffset );
-BALL_DLL_IMPORT_C void *_aligned_realloc( void *p, unsigned long long nSize, unsigned long long nAlignment );
+#	if defined( BALL_MSVC )
+BALL_EXTERN_C __declspec( restrict ) void * __cdecl _aligned_malloc( unsigned long long nSize, unsigned long long nAlignment );
+BALL_EXTERN_C void __cdecl _aligned_free( void *p );
+BALL_EXTERN_C unsigned long long __cdecl _aligned_msize( void *p, unsigned long long nAlignment, unsigned long long nOffset );
+BALL_EXTERN_C __declspec( restrict ) void * __cdecl _aligned_realloc( void *p, unsigned long long nSize, unsigned long long nAlignment );
+#	else
+BALL_EXTERN_C void *_aligned_malloc( unsigned long long nSize, unsigned long long nAlignment );
+BALL_EXTERN_C void _aligned_free( void *p );
+BALL_EXTERN_C unsigned long long _aligned_msize( void *p, unsigned long long nAlignment, unsigned long long nOffset );
+BALL_EXTERN_C void *_aligned_realloc( void *p, unsigned long long nSize, unsigned long long nAlignment );
+#	endif // defined( BALL_MSVC )
 
 #endif // !defined( _INCLUDE_BALL_TYPES_C_MEMORYALIGNED_H_ )
